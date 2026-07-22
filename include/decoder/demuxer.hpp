d496e28b6af6e5f3411ad3ca00f2a4fc3db81b22
quaslir <karl33060@gmail.com>
2026-07-22 14:32:22 +0200

    demuxer class finished

--- include/decoder/demuxer.hpp
+++ include/decoder/demuxer.hpp
@@ -1,6 +1,12 @@
 #pragma once
 
+#include <cstdint>
+#include <cstdio>
+#include <libavcodec/codec_id.h>
+#include <libavcodec/packet.h>
+#include <libavutil/rational.h>
 #include <memory>
+#include <optional>
 #include <string>
 extern "C" {
 #include <libavcodec/avcodec.h>
@@ -17,13 +23,63 @@ struct AVFormatContextDeleter {
     }
 };
 
+
 using smart_format_context = std::unique_ptr<AVFormatContext,AVFormatContextDeleter >;
+
+struct AVPacketDeleter {
+    void operator ()(AVPacket * packet) const{
+        if(packet) {
+            av_packet_free(&packet);
+        }
+    }
+};
+
+using smart_packet = std::unique_ptr<AVPacket, AVPacketDeleter>;
+
+struct VideoInfo {
+    AVCodecID codec_id;
+    int width, height;
+    AVPixelFormat format;
+    int64_t bit_rate;
+    AVRational framerate;
+};
+
+struct AudioInfo {
+    AVCodecID codec_id;
+    int sample_rate;
+    AVSampleFormat format;
+    int channels;
+    int64_t bit_rate;
+};
+
+enum class PacketType {
+    VIDEO,
+    AUDIO,
+    OTHER,
+    ERROR
+};
+
+struct DemuxedPacket {
+    PacketType type;
+    smart_packet packet;
+};
+
 class Demuxer {
   private:
     smart_format_context format_context{nullptr};
 
-    int video_stream_index{0};
+    int video_stream_index{-1};
+    int audio_stream_index{-1};
   public:
       Demuxer();
       bool open(const std::string& filename);
+      bool is_open() const;
+
+      bool has_video() const;
+      bool has_audio() const;
+
+      std::optional<VideoInfo> video_stream_info() const;
+      std::optional<AudioInfo> audio_stream_info() const;
+
+      std::optional<DemuxedPacket> read_next_packet();
 };
--- src/decoder/demuxer.cpp
+++ src/decoder/demuxer.cpp
@@ -1,9 +1,13 @@
 #include "decoder/demuxer.hpp"
+#include <algorithm>
 #include <libavcodec/avcodec.h>
 #include <libavcodec/codec.h>
 #include <libavcodec/codec_par.h>
+#include <libavcodec/packet.h>
 #include <libavformat/avformat.h>
 #include <libavutil/avutil.h>
+#include <libavutil/error.h>
+#include <optional>
 
 Demuxer::Demuxer() : format_context(avformat_alloc_context()) {}
 
@@ -24,7 +28,70 @@ if(video_index < 0) return false;
 
 video_stream_index = video_index;
 
+int audio_index = av_find_best_stream(format_context.get(), AVMEDIA_TYPE_AUDIO, -1, -1, nullptr, 0);
+
+audio_stream_index = audio_index;
 
 return true;
+
+}
+
+
+bool Demuxer::is_open() const {
+    return format_context.get() != nullptr;
+}
+
+bool Demuxer::has_video() const {
+    return video_stream_index >= 0;
+}
+bool Demuxer::has_audio() const {
+    return audio_stream_index >= 0;
+}
+
+std::optional<VideoInfo> Demuxer::video_stream_info() const {
+    if(!has_video()) return std::nullopt;
+
+    VideoInfo video_info;
+    AVCodecParameters* codecpar = format_context->streams[video_stream_index]->codecpar;
+    video_info.codec_id = codecpar->codec_id;
+    video_info.format = static_cast<AVPixelFormat>(codecpar->format);
+    video_info.width = codecpar->width;
+    video_info.height = codecpar->height;
+    video_info.bit_rate = codecpar->bit_rate;
+    video_info.framerate = codecpar->framerate;
+    return video_info;
+}
+
+std::optional<AudioInfo> Demuxer::audio_stream_info() const {
+if(!has_audio()) return std::nullopt;
+
+AudioInfo audio_info;
+AVCodecParameters * codecpar = format_context->streams[audio_stream_index]->codecpar;
+
+audio_info.codec_id = codecpar->codec_id;
+audio_info.sample_rate = codecpar->sample_rate;
+audio_info.format = static_cast<AVSampleFormat>(codecpar->format);
+audio_info.bit_rate = codecpar->bit_rate;
+audio_info.channels =  codecpar->ch_layout.nb_channels;
+return audio_info;
+}
+
+std::optional<DemuxedPacket> Demuxer::read_next_packet() {
+    smart_packet packet{av_packet_alloc()};
+    int result = av_read_frame(format_context.get(), packet.get());
+
+    if(result == AVERROR_EOF) {
+        return std::nullopt;
+    }
+
+    if(result < 0) {
+        return DemuxedPacket(PacketType::ERROR, std::move(packet));
+    }
+    PacketType packet_type;
+    if(packet->stream_index == video_stream_index) packet_type = PacketType::VIDEO;
+    else if(packet->stream_index == audio_stream_index) packet_type = PacketType::AUDIO;
+    else packet_type = PacketType::OTHER;
+
+    return DemuxedPacket(packet_type, std::move(packet));
 
 }
